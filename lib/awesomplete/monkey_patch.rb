55c974deef2a09a9cb70813222a5d948135c840f
will <willjbarker@gmail.com>
2015-08-18 11:58:33 +0100

    value for input
    a value

--- lib/awesomplete/monkey_patch.rb
+++ lib/awesomplete/monkey_patch.rb
@@ -2,7 +2,7 @@
   scope.class_eval do
     define_method :awesomplete_field do |name, options|
       <<-INP.squish.html_safe
-      <input class='awesomplete' name='#{name}' list='#{name}_list' >
+      <input class='awesomplete' value="#{options[:value]}"  name='#{name}' list='#{name}_list' >
       <datalist id='#{name}_list'>
         #{options[:collection].map { |i| '<option>'+ i +'</option>' }}
       </datalist>
