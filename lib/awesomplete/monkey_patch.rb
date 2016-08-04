f905550a4214c23713cd44bd4bbe18a33adfc736
vndatech <jan.vereecke@vandapharma.com>
2016-08-04 16:51:06 -0400

    Update monkey_patch.rb

--- lib/awesomplete/monkey_patch.rb
+++ lib/awesomplete/monkey_patch.rb
@@ -4,7 +4,7 @@
       <<-INP.squish.html_safe
       <input class='awesomplete' value="#{options[:value]}"  name='#{name}' list='#{name}_list' >
       <datalist id='#{name}_list'>
-        #{options[:collection].map { |i| '<option value="'+i[:value]'">'+ i[:label] +'</option>' }}
+        #{options[:collection].map { |i| '<option value="'+i[:value]+'">'+ i[:label] +'</option>' }}
       </datalist>
       INP
     end
